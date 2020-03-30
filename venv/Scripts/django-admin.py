79a845a6b8e31c8bc97ac0168567eea5345dabe9
Alex <alex18812649207@gmail.com>
2020-03-30 22:50:57 +0800

    2020-3-30

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!G:\Python\PythonFullStack\第七模块_CRM+爬虫开发\MatrixCRM\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
